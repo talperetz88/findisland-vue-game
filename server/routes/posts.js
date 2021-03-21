b815d83e3e64c2375be07ec7316b92ff123631ee
talperetz88 <talperetz88@gmail.com>
2021-03-21 21:25:17 +0200

    Add files via upload

---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// router.get('/',(req,res) =>{
+//     res.send("Hello worlds");
+// })
+
+router.get('/vue-game',(req,res) =>{
+    res.send();
+})
+
+module.exports = router;
